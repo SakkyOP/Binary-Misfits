72f9f92b3190886e83fece3e2b8c8c732069c92d
SakkyOP <saksham298@gmail.com>
2022-03-26 20:22:55 +0530

    linking commit

---
+++ src/contact.js
@@ -0,0 +1,16 @@
+
+var submitButton = document.getElementById("contact-submit");
+
+submitButton.addEventListener("click",event=>{
+    let [nameBox,emailBox,subjectBox,messageBox] = document.getElementsByClassName("textbox");
+    let obj = {
+        name : nameBox.value,
+        email : emailBox.value,
+        subject : subjectBox.value,
+        message : messageBox.value  
+    }
+    console.log(obj);
+    [...document.getElementsByClassName("textbox")].forEach(element => {
+        element.value = "";
+    });
+});
