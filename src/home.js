acfdf3203e1ff4d8ed841815ff6c6707b04ed98d
ZODIAC3K <88622014+ZODIAC3K@users.noreply.github.com>
2022-03-29 11:52:00 +0530

    fixed nav bar js

--- src/home.js
+++ src/home.js
@@ -1,16 +1,18 @@
-const hamburger = document.getElementById('hamburger');
-const navUL = document.getElementById('nav-ul-mobile');
-const mobile = document.getElementById('mobile');
-const close = document.getElementById('close');
+var hamburger = document.getElementById('hamburger');
+var navUL = document.getElementById('nav-ul-mobile');
+var mobile = document.getElementById('mobile');
+var close = document.getElementById('close');
+var body = document.getElementById('body');
 
 hamburger.addEventListener('click', () => {
+    body.style.overflow = "hidden";
     navUL.classList.toggle('show');
     mobile.classList.toggle('show');
-
 });
 
 close.addEventListener('click' , ()=> {
     navUL.classList.toggle('show');
     mobile.classList.toggle('show');
+    body.style.overflow = "auto";
 });
 
