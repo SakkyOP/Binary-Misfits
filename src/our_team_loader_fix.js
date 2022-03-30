4081405784167f5fc3d040119990ca9fbec3404e
ZODIAC3K <88622014+ZODIAC3K@users.noreply.github.com>
2022-03-30 10:55:04 +0530

    fixed most things after update

--- src/our_team_loader_fix.js
+++ src/our_team_loader_fix.js
@@ -1,8 +1,12 @@
 
 var loader = document.getElementById('loader');
 var body = document.getElementById('body');
+var btn = document.getElementById('backbtn');
+
+console.log(btn);
 
 window.addEventListener("load", function() { setTimeout(function(){
+    btn.style.display= "flex";
     loader.style.display = "none";
     body.style.overflow = "auto";
 },2000);
